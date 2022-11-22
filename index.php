6e7c6198cd92cf6f9cd7484444c83bbcd7ca2101
cadalzolc <cadalzolc@gmail.com>
2022-11-22 08:08:56 +0800

    added styles and base pages

--- index.php
+++ index.php
@@ -1,5 +1,48 @@
-hello ;)
+<?php
 
-welcome to my mobile legends
+session_start();
 
-first blood
+include('./libs/base.php');
+include('./libs/db.php');
+
+$today = date("D, M j, Y");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <title>Home</title>
+    <link rel="icon" href="<?php echo BASE_URL() . 'assets/base/img/icon.png' ?>" type="image/png" sizes="16x16">
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/fontface' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/unicons.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/style.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/responsive.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/night-mode.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/all.min.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/owl.carousel.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/owl.theme.default.min.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/bootstrap.min.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/bootstrap-select.min.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/custom.css' ?>">
+</head>
+<body class="d-flex flex-column h-100">
+
+    <?php include("./website/layouts/header.php"); ?>
+
+   
+    <script src="<?php echo BASE_URL() . 'assets/base/js/jquery-3.6.0.min.js' ?>"></script>
+    <script src="<?php echo BASE_URL() . 'assets/base/js/bootstrap.bundle.min.js' ?>"></script>
+    <script src="<?php echo BASE_URL() . 'assets/base/js/owl.carousel.js' ?>"></script>
+    <script src="<?php echo BASE_URL() . 'assets/base/js/bootstrap-select.min.js' ?>"></script>
+    <script src="<?php echo BASE_URL() . 'assets/base/js/custom.js' ?>"></script>
+    <script src="<?php echo BASE_URL() . 'assets/base/js/night-mode.js' ?>"></script>
+    <script src="<?php echo BASE_URL() . 'assets/base/js/app.js' ?>"></script>
+</body>
+</html>
