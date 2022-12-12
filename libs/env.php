65db0cd0df36301018296fec0d16aef1fd345602
cadalzolc <cadalzolc@gmail.com>
2022-12-13 07:24:53 +0800

    fixed config environment

--- libs/env.php
+++ libs/env.php
@@ -7,10 +7,11 @@ class Configuration {
     {
         if(empty(Configuration::$environment))
         {
-            switch($_SERVER['SERVER_ADDR'])
+            switch($_SERVER['SERVER_NAME'])
             {
-                case '151.106.117.3':
-                    Configuration::$environment = 'prod'; break;
+                case 'speshvenue.com':
+                case 'speshvenue-com.preview-domain.com':
+                        Configuration::$environment = 'prod'; break;
                 case '2a02:4780:3:545:0:1cee:3931:2':
                     Configuration::$environment = 'prod'; break;
                 default:
