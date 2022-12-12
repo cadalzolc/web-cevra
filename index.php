16cc6682f603597b5d446524696d8f591ccbb734
cadalzolc <cadalzolc@gmail.com>
2022-12-13 07:31:48 +0800

    fixed config environment

--- index.php
+++ index.php
@@ -7,12 +7,12 @@ include('./libs/base.php');
 include('./libs/db.php');
 include('./libs/func.php');
 
-$today = date("D, M j, Y");
+//$today = date("D, M j, Y");
 
-$sql_venues = "SELECT * FROM vw_listing LIMIT 12";
-$db = new Server();
-$qry_venues = $db->DbQuery($sql_venues);
-$cntLst = mysqli_num_rows($qry_venues);
+//$sql_venues = "SELECT * FROM vw_listing LIMIT 12";
+//$db = new Server();
+//$qry_venues = $db->DbQuery($sql_venues);
+//$cntLst = mysqli_num_rows($qry_venues);
 
 ?>
 
@@ -43,10 +43,10 @@ $cntLst = mysqli_num_rows($qry_venues);
 </head>
 
 <body class="d-flex flex-column h-100">
-    <pre><?php echo Configuration::Setting('server'); ?></pre>
-    <pre> <?php echo Configuration::Setting('username'); ?></pre>
-    <pre><?php echo Configuration::Setting('password'); ?></pre>
-    <pre><?php echo Configuration::Setting('database'); ?></pre>
+    <pre>Server:    <?php echo Configuration::Setting('server'); ?></pre>
+    <pre>Username:  <?php echo Configuration::Setting('username'); ?></pre>
+    <pre>Password:  <?php echo Configuration::Setting('password'); ?></pre>
+    <pre>Database:  <?php echo Configuration::Setting('database'); ?></pre>
 </body>
 
 </html>
