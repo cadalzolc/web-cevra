04f36df2d64a175b59f3e0a5011d3c450569640a
cadalzolc <cadalzolc@gmail.com>
2022-12-13 05:52:24 +0800

    Added environment settings

--- libs/db.php
+++ libs/db.php
@@ -1,21 +1,22 @@
 <?php
+require_once('env.php');
 class Server
 {
-
-  public $server = "localhost";
-  public $database = "db_events";
-  public $username = "root";
-  public $password = "";
-
-  function DbQuery($Sql)
-  {
-    $conn = new mysqli($this->server, $this->username, $this->password, $this->database);
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    } else {
-      return $conn->query($Sql);
+    function DbQuery($Sql)
+    {
+        $conn = new mysqli(
+            Configuration::Setting('server'),
+            Configuration::Setting('username'),
+            Configuration::Setting('password'),
+            Configuration::Setting('database')
+        );
+        if ($conn->connect_error) 
+        {
+            die("Connection failed: " . $conn->connect_error);
+        } else 
+        {
+            return $conn->query($Sql);
+        }
     }
-  }
 }
-
 ?>
