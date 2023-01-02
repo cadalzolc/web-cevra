650220940e7ca5a9bbe146e41370b25812310f0e
cadalzolc <cadalzolc@gmail.com>
2023-01-02 21:14:31 +0800

    business profile

---
+++ business/profile.php
@@ -0,0 +1,95 @@
+<?php 
+
+session_start(); 
+
+include('../libs/base.php');
+include('../libs/func.php');
+include('../libs/db.php');
+
+if (empty($_SESSION['B-ID'])) {
+    header("Location: " . BASE_URL() . 'business/login.php');
+    exit;
+}
+
+$GLOBALS["tabs"] = "Profile";
+
+$owner = $_SESSION['B-ID'];
+$sql = "SELECT * FROM vw_resevation WHERE status != 'FV' AND business_id = $owner";
+$db = new Server();
+$rows = $db->DbQuery($sql);
+$cnt = mysqli_num_rows($rows);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <title>Business - Profile</title>
+    <link rel="icon" href="<?php echo BASE_URL() . 'assets/base/img/icon.png' ?>" type="image/png" sizes="16x16">
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/dashboard.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/fontface' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/unicons.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/style.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/vertical-responsive-menu.min.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/responsive.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/night-mode.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/all.min.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/owl.carousel.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/owl.theme.default.min.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/bootstrap.min.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/bootstrap-select.min.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/base/css/custom.css' ?>">
+    <link rel="stylesheet" href="<?php echo BASE_URL() . 'assets/plugins/css/toastr.css' ?>">
+</head>
+
+<body class="d-flex flex-column h-100">
+    <?php include("./layouts/header.php"); ?>
+    <?php include("./layouts/sidebar.php"); ?>
+    <div class="wrapper wrapper-body" id="vbody">
+        <div class="dashboard-body">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="d-main-title">
+                            <h3><i class="fas fa-bookmark me-3"></i></i>Profile</h3>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="main-card mt-4 p-4 d-flex justify-content-between align-items-center">
+                            <div class="bp-info">
+                                <h4>Business Information</h4>
+                                <p class="mb-0">No payment information added yet.</p>
+                            </div>
+                            <button class="pe-4 ps-4 text-center co-main-btn h_40 d-inline-block" >Edit</button>
+                        </div>
+                        <div class="main-card mt-4 p-4 d-flex justify-content-between align-items-center">
+                            <div class="bp-info">
+                                <h4>Payment Information</h4>
+                                <p class="mb-0">No payment information added yet.</p>
+                            </div>
+                            <button class="pe-4 ps-4 text-center co-main-btn h_40 d-inline-block" data-bs-toggle="modal" data-bs-target="#paymentinfoModal">Edit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="<?php echo BASE_URL() . 'assets/base/js/jquery-3.6.0.min.js' ?>"></script>
+    <script src="<?php echo BASE_URL() . 'assets/base/js/bootstrap.bundle.min.js' ?>"></script>
+    <script src="<?php echo BASE_URL() . 'assets/base/js/owl.carousel.js' ?>"></script>
+    <script src="<?php echo BASE_URL() . 'assets/base/js/bootstrap-select.min.js' ?>"></script>
+    <script src="<?php echo BASE_URL() . 'assets/base/js/custom.js' ?>"></script>
+    <script src="<?php echo BASE_URL() . 'assets/base/js/night-mode.js' ?>"></script>
+    <script src="<?php echo BASE_URL() . 'assets/base/js/app.js' ?>"></script>
+    <script src="<?php echo BASE_URL() . 'assets/plugins/js/toastr.js' ?>"></script>
+    <script src="<?php echo BASE_URL() . 'business/js/app.js' ?>"></script>
+</body>
+
+</html>
