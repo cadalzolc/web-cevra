4cb7248753fd13b9ad188b6f83e9be3f086b0994
cadalzolc <cadalzolc@gmail.com>
2023-01-06 10:40:58 +0800

    business-update

--- admin/forms/form-viewer.php
+++ admin/forms/form-viewer.php
@@ -15,7 +15,7 @@ $row_user = mysqli_fetch_array($res_user);
         <div class="row justify-content-center">
             <button class="btn btn-primary" style="position: fixed; z-index: 100; top: 20px; right: 27px; width: 100px;"
             data-dialog-close="#<?php echo $_GET['div'] ?>">Close PDF</button>
-            <iframe src="<?php echo BASE_URL(). 'assets/uploads/docs/'. $row_user['proof'] . '#view=FitH';  ?>" 
+            <iframe src="<?php echo BASE_URL(). 'assets/uploads/docs/'. $row_user['proof'];  ?>" 
                 frameborder="0" height="100%" width="100%" style="position: absolute;">
             </iframe>
         </div>
